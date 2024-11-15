de6db0c6a91dde9fbc88aab5b3b9dfb9f5657395
mmack234 <mmackillip@gmail.com>
2024-11-15 15:43:41 -0600

    Update page.tsx
    Adding header div
    Including header text and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <p>Brand new app stuff</p>
+    <main className="flex min-h-screen flex-col">
+      <div className="h-9 px-2 flex justify-between items-center" style={{backgroundColor: "#58686B"}}>
+        <p style={{color: "#ffffff"}} className="font-sans">Sort</p>
+        <p style={{color: "#ffffff"}} className="font-sans">Recipes</p>
+        <button style={{
+      // borderRadius: '50%', // Makes the button circular
+      padding: '6px',
+      backgroundColor: '#009d94',
+      color: '#ffffff',
+      border: 'none'
+    }}>
+      +
+    </button>
+      </div>
     </main>
   );
 }
