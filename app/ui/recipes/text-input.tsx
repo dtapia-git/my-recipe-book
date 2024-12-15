db65b23fc76321fefa0579610e9cc7d641856252
mmack234 <mmackillip@gmail.com>
2024-12-15 14:21:44 -0600

    Update text-input.tsx
    adding change and cancel functions to input

--- app/ui/recipes/text-input.tsx
+++ app/ui/recipes/text-input.tsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 
 export default function TextInput() {
-    let [isFocused, setIsFocused] = useState(true);
+    let [inputValue, setInputValue] = useState('');
+    let [isFocused, setIsFocused] = useState(false);
 
    function handleOnFocus() {
     setIsFocused(true);
    }
 
+   function handleBlur() {
+    setIsFocused(false);
+   }
+
+   function handleChange(event: any) {
+    setInputValue(event.target.value);
+   }
+
+   function handleCancel() {
+    setInputValue('');
+    setIsFocused(false);
+   }
+
     return (
         <div>
-            <p>hello</p>
-            <input type="text" name="textInput" onFocus={handleOnFocus}></input>
-            <div>{isFocused ? 
-            (<div className="flex justify-between"><button>Cancel</button><button>Done</button></div>) 
-            : 
-            (<div></div>)}</div>
+            <input type="text" name="textInput" value={inputValue} onFocus={handleOnFocus} onChange={handleChange} className="bg-slate-100 border-0 w-full"></input>
+            {isFocused && <div className="flex justify-between border-black border"><button onClick={handleCancel}>Cancel</button><button onClick={handleBlur} style={{color: "#009d94"}}>Done</button></div>}
         </div>
     );
 }
