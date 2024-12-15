f4cab0cc35e881f5f45a1452809e4cf4ac5cdd7a
mmack234 <mmackillip@gmail.com>
2024-12-15 11:19:26 -0600

    db seed
    add seed function

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -2,28 +2,34 @@ import { Recipe } from "./definitions";
 
 const recipes: Recipe[] = [
   {
-    id: "1",
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: "Burgers",
     calories: 650,
     protein: 30,
     carbohydrates: 40,
     fat: 20,
+    ingredients: ['1lb ground beef', 'pickles', 'buns'],
+    directions: ['cook beef patties', 'toast buns', 'assemble']
   },
   {
-    id: "2",
+    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
     name: "Spaghetti",
     calories: 800,
     protein: 30,
     carbohydrates: 45,
     fat: 15,
+    ingredients: ['1lb ground beef', 'pickles', 'buns'],
+    directions: ['cook beef patties', 'toast buns', 'assemble']
   },
   {
-    id: "3",
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: "Enchilads",
     calories: 960,
     protein: 27,
     carbohydrates: 45,
     fat: 25,
+    ingredients: ['1lb ground beef', 'pickles', 'buns'],
+    directions: ['cook beef patties', 'toast buns', 'assemble']
   },
 ];
 
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -5,7 +5,7 @@ const client = await db.connect();
 
 async function seedRecipes() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-  await client.sql`
+  const createTable = await client.sql`
     CREATE TABLE IF NOT EXISTS recipes (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
@@ -21,26 +21,26 @@ async function seedRecipes() {
   const insertedRecipes = await Promise.all(
     recipes.map(async (recipe) => {
       return client.sql`
-        INSERT INTO recipes (id, name, calories, protein, carbohydrates, fat)
-        VALUES (${recipe.id}, ${recipe.name}, ${recipe.calories}, ${recipe.protein}, ${recipe.carbohydrates}, ${recipe.fat})
+        INSERT INTO recipes (id, name, calories, protein, carbohydrates, fat, ingredients, directions)
+        VALUES (${recipe.id}, ${recipe.name}, ${recipe.calories}, ${recipe.protein}, ${recipe.carbohydrates}, ${recipe.fat}, ARRAY['ingredient 1, ingredient 2, ingredient 3'], ARRAY['direction 1, direction 2, direction 3'])
         ON CONFLICT (id) DO NOTHING;
       `;
     }),
   );
 
   return insertedRecipes;
+
+  // return client.sql `DROP TABLE recipes`;
 }
 
 export async function GET() {
     try {
-      console.log('Seeding DB')
       await client.sql`BEGIN`;
       await seedRecipes();
       await client.sql`COMMIT`;
   
       return Response.json({ message: 'Database seeded successfully' });
     } catch (error) {
-      console.log('Seeding DB FAILED')
       await client.sql`ROLLBACK`;
       return Response.json({ error }, { status: 500 });
     }
