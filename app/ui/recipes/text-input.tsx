442e1da51653c5a04415574bcfd817acb1b83cb0
dtapia-git <danielt.atx@gmail.com>
2024-12-26 15:40:13 -0600

    error fix
    add use client

--- app/ui/recipes/text-input.tsx
+++ app/ui/recipes/text-input.tsx
@@ -1,30 +1,45 @@
+"use client";
+
 import { useState } from "react";
 
 export default function TextInput() {
-    let [inputValue, setInputValue] = useState('');
-    let [isFocused, setIsFocused] = useState(false);
+  let [inputValue, setInputValue] = useState("");
+  let [isFocused, setIsFocused] = useState(false);
 
-   function handleOnFocus() {
+  function handleOnFocus() {
     setIsFocused(true);
-   }
+  }
 
-   function handleBlur() {
+  function handleBlur() {
     setIsFocused(false);
-   }
+  }
 
-   function handleChange(event: any) {
+  function handleChange(event: any) {
     setInputValue(event.target.value);
-   }
+  }
 
-   function handleCancel() {
-    setInputValue('');
+  function handleCancel() {
+    setInputValue("");
     setIsFocused(false);
-   }
+  }
 
-    return (
-        <div>
-            <input type="text" name="textInput" value={inputValue} onFocus={handleOnFocus} onChange={handleChange} className="bg-slate-100 border-0 w-full"></input>
-            {isFocused && <div className="flex justify-between border-black border"><button onClick={handleCancel}>Cancel</button><button onClick={handleBlur} style={{color: "#009d94"}}>Done</button></div>}
+  return (
+    <div>
+      <input
+        type="text"
+        name="textInput"
+        value={inputValue}
+        onFocus={handleOnFocus}
+        onChange={handleChange}
+        className="bg-slate-100 border-0 w-full"></input>
+      {isFocused && (
+        <div className="flex justify-between border-black border">
+          <button onClick={handleCancel}>Cancel</button>
+          <button onClick={handleBlur} style={{ color: "#009d94" }}>
+            Done
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
